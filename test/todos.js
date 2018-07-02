74493ff32875da6418a52d08a929bc066bae16c7
Brad Bohen <bbohen@gmail.com>
2018-07-02 07:54:08 -0400

    Updated actual todos

--- test/todos.js
+++ test/todos.js
@@ -3,6 +3,6 @@
 // TODO: add some colors
 // TODO: add list format for lengthy todos (or something similar)
 // TODO: electron app
-// TODO: generate markdown of some sort
+// TODO: generate markdown of some sort, auto add this file to repo
 // TODO: create GH issues from specific todos
 // TODO: create peristent data store and sort/flag by age
