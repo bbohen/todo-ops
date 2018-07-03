9b6e20361109a560568c19c4f05c3db0b584103b
Brad Bohen <bbohen@gmail.com>
2018-07-03 07:01:16 -0400

    Updated actual todos

--- test/todos.js
+++ test/todos.js
@@ -1,7 +1,7 @@
 // Actual TODOs for the project
 
+// TODO: generate markdown of todos broken down by file (anything else?)
 // TODO: add list format for lengthy todos (or something similar)
+// TODO: create GH issues from specific todos using terminal interface (maybe electron should come first?)
 // TODO: electron app
-// TODO: generate markdown of some sort, auto add this file to repo
-// TODO: create GH issues from specific todos
 // TODO: create peristent data store and sort/flag by age
